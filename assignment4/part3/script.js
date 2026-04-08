cdfe77bf5111a0fe3fee30a5f1434993791dc7b7
karolinaagnieszka <karolina.blok@dcmail.ca>
2026-04-08 14:21:34 -0400

    adding initial files

---
+++ assignment4/part3/script.js
@@ -0,0 +1,25 @@
+//Name: Karolina Blok
+//File: assignment4part3
+//Date: 10 April 2026
+// this file includes object building practice
+
+
+// setup canvas
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
