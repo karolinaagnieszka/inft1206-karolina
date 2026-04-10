3beab882261a4d919a058c104cc787c8b64811f4
karolinaagnieszka <karolina.blok@dcmail.ca>
2026-04-09 21:47:15 -0400

    adding shape class, and updating collision detect method

--- assignment4/part4/script.js
+++ assignment4/part4/script.js
@@ -24,6 +24,25 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
+class Shape {
+  constructor(x, y, velX, velY) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+  }
+
+}
+
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
+    this.color = color;
+    this.size = size;
+    this.exists = true;
+  }
+}
+
 class Ball {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
@@ -64,7 +83,7 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (this !== ball) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -75,7 +94,9 @@ class Ball {
       }
     }
   }
+  // …
 }
+
 const balls = [];
 
 while (balls.length < 25) {
