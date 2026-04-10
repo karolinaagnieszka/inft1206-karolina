7041fafe214db48954b2b73619b0a72ca88baa48
karolinaagnieszka <karolina.blok@dcmail.ca>
2026-04-10 16:37:16 -0400

    final changes and tweaks

--- assignment4/part2/script.js
+++ assignment4/part2/script.js
@@ -11,25 +11,30 @@ const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
 
-const images = [
-    { filename: "pic1.jpg", alt: "Closeup of a human eye"},
-    { filename: "pic2.jpg", alt: "Rock that looks like a wave"},
-    { filename: "pic3.jpg", alt: "Purple and white pansies"},
-    { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb"},
-    { filename: "pic5.jpg", alt: "Large moth on a leaf"},
-];
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const alts = {
+    "pic1.jpg": "Closeup of a human eye",
+    "pic2.jpg": "Rock that looks like a wave",
+    "pic3.jpg": "Purple and white pansies",
+    "pic4.jpg": "Section of wall from a pharaoh's tomb",
+    "pic5.jpg": "Large moth on a leaf"
+};
 
 const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
 
-for (const image of images) {
+for (let i = 0; i < images.length; i++) {
 
     const newImage = document.createElement("img");
-    newImage.src = `${baseURL}${image.filename}`;
-    newImage.alt = image.alt;
-    newImage.tabIndex = "0";
+
+    newImage.setAttribute("src", `${baseURL}${images[i]}`);
+    newImage.setAttribute("alt", alts[images[i]]);
 
     thumbBar.appendChild(newImage);
-    newImage.addEventListener("click", updateDisplayedImage);
+    newImage.addEventListener("click", (e) => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+
+    });
 
 }
 
@@ -39,14 +44,16 @@ function updateDisplayedImage(e) {
 }
 
 btn.addEventListener("click", () => {
-    if (btn.classList.contains("dark")) {
+    const btnClass = btn.getAttribute("class");
+    if (btnClass == "dark") {
+        btn.setAttribute("class", "light");
         btn.textContent = "Lighten";
-        overlay.style.backgroundColor = "rgb(0 0 0 / 0.5)"; }
-        else {
-            btn.textContent = "Darken";
-            overlay.style.backgroundColor = "rgb(0 0 0 / 0)";
-
-        }
-        btn.classList.toggle("dark");
+        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    }
+    else {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    }
 
 });
